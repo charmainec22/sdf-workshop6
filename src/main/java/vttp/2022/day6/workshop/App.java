2525072dcdab46242da9937be8f9157d49f96e4c
charmainec22 <charmainechiashuting@gmail.com>
2022-06-20 13:52:34 +0800

    first

---
+++ src/main/java/vttp/2022/day6/workshop/App.java
@@ -0,0 +1,13 @@
+package vttp.2022.day6.workshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
